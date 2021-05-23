43e9c99e8926903d317201398b8b506c747d6a43
Adam Cameron <cameron.adam+gitkraken@gmail.com>
2021-05-23 12:46:37 +0100

    fixing restart script & adding new table to DB insatll scripts

--- docker/restartContainers.sh
+++ docker/restartContainers.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 clear; printf "\033[3J"
 docker-compose stop
-MARIADB_ROOT_PASSWORD=$1 MARIADB_PASSWORD=$2 docker-compose start --detach
+MARIADB_ROOT_PASSWORD=$1 MARIADB_PASSWORD=$2 docker-compose up --detach mariadb
+MARIADB_PASSWORD=$2 docker-compose up --detach lucee
+docker-compose start nginx
